96d30ef9db6dcf70b03acfbe5164ac6895d1fd85
stoykova <alstoykovana@gmail.com>
2023-03-11 20:53:15 +0000

    files are modified

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import qrcode
-img = qrcode.make('GitHub Webhook')
-img.save("gitHub_webhook.png")
+import base64
+from flask import Flask
+from flask import request
+from io import BytesIO
+
+app = Flask(__name__)
+
+@app.route("/qr")
+def qr():
+   msg = request.args.get('msg')
+   img = qrcode.make(msg)
+   
+   buffer = BytesIO()
+   img.save(buffer, format="png")
+ 
+   img64 = base64.b64encode(buffer.getvalue())
+   return f'<img src="data:image/png;base64, {img64.decode()}" alt="qrcode" />'
+
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0')
